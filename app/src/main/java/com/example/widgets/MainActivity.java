d458ff20cfe99ff79e9202770c156551a6a076cb
a19jimsa <a19jimsa@student.his.se>
2021-04-08 12:00:03 +0200

    Added EditText and added linearLayout. Added button to add mountain to arrayadapter.

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -15,19 +18,37 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
     private String[] mountainNames = {"Matterhorn", "Mont Blanc","Denali"};
     private ArrayList<String> listData = new ArrayList<String>(Arrays.asList(mountainNames));
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item_textview,
-                R.id.list_item_textview,listData);
-        ListView my_listview = (ListView) findViewById(R.id.my_listview);
-        my_listview.setAdapter(adapter);
-        my_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item_textview,
+                R.id.list_item_textView,listData);
+        final ListView my_listView = (ListView) findViewById(R.id.my_listView);
+        my_listView.setAdapter(adapter);
+        my_listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(getApplicationContext(), "Bla bla", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        editText = findViewById(R.id.editText);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Hej","Du har tryck på knappen!");
+                if(editText.getText().length() != 0){
+                    listData.add(editText.getText().toString());
+                    my_listView.setAdapter(adapter);
+                    Toast.makeText(getApplicationContext(), "Added new Mountain", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Field can not be empty", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
